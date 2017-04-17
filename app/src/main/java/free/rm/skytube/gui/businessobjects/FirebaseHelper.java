502aa79021f0d032314fb7b8809a17100c1432f9
César Rodrigues <rasec1287@hotmail.com>
2017-04-17 12:22:38 -0300

    Atualizado arquivo de ajuda do Firebase com novos campos do Database

--- app/src/main/java/free/rm/skytube/gui/businessobjects/FirebaseHelper.java
+++ app/src/main/java/free/rm/skytube/gui/businessobjects/FirebaseHelper.java
@@ -8,4 +8,7 @@ public class FirebaseHelper {
     public static final String FIREBASE_DATABASE_ID_CHANNEL = "id_canal";
     public static final String FIREBASE_DATABASE_IMPRESSIONS = "impressoes";
     public static final String FIREBASE_DATABASE_AD = "anuncio";
+    public static final String FIREBASE_DATABASE_DOUBTS = "duvidas";
+    public static final String FIREBASE_DATABASE_QUESTION = "pergunta";
+    public static final String FIREBASE_DATABASE_ANSWER = "resposta";
 }
