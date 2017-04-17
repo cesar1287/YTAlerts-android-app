5ad756050c4ec3ccfe2de5b2444878e15710e69f
César Rodrigues <rasec1287@hotmail.com>
2017-04-17 12:18:19 -0300

    Refatorado caminhos de algumas classes de domínio

---
+++ app/src/main/java/free/rm/skytube/gui/businessobjects/domain/DoubtFirebase.java
@@ -0,0 +1,22 @@
+package free.rm.skytube.gui.businessobjects.domain;
+
+public class DoubtFirebase {
+
+    private String pergunta, resposta;
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+}
