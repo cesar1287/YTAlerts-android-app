8b981e5d7c66209f7bb3ba32ac80664883dd6ca6
César Rodrigues <rasec1287@hotmail.com>
2017-04-17 12:10:23 -0300

    Adicionada tela de Sobre

---
+++ app/src/main/java/free/rm/skytube/gui/activities/AboutActivity.java
@@ -0,0 +1,15 @@
+package free.rm.skytube.gui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import free.rm.skytube.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
