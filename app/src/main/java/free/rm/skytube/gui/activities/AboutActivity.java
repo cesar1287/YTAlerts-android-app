5902d98a8fdda75d45216bb3ccd69df10798b1a4
César Rodrigues <rasec1287@hotmail.com>
2017-04-18 09:08:53 -0300

    Atualizada tela de Sobre com a implementação dos emails pelo firebase

--- app/src/main/java/free/rm/skytube/gui/activities/AboutActivity.java
+++ app/src/main/java/free/rm/skytube/gui/activities/AboutActivity.java
@@ -1,15 +1,108 @@
 package free.rm.skytube.gui.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import free.rm.skytube.R;
+import free.rm.skytube.gui.businessobjects.ExpandableListAdapter;
+import free.rm.skytube.gui.businessobjects.FirebaseHelper;
 
 public class AboutActivity extends AppCompatActivity {
+
+    Query about;
+    ValueEventListener valueEventListener;
+    ValueEventListener singleValueEventListener;
+
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        dialog = ProgressDialog.show(this,"", this.getResources().getString(R.string.loading_about_pls_wait), true, false);
+
+        setupAbout();
+
+        // display the back button in the action bar (left-hand side)
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.about);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        about.removeEventListener(valueEventListener);
+
+        about.removeEventListener(singleValueEventListener);
+    }
+
+    private void setupAbout() {
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        about = mDatabase.child(FirebaseHelper.FIREBASE_DATABASE_ABOUT);
+
+        valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+
+                    TextView emailIcon = (TextView) findViewById(R.id.email_developer_icon);
+                    emailIcon.setText((String)postSnapshot.child(FirebaseHelper.FIREBASE_DATABASE_EMAIL_ICON).getValue());
+
+                    TextView emailYTAlerts = (TextView) findViewById(R.id.email_proper_ytalerts);
+                    emailYTAlerts.setText((String)postSnapshot.child(FirebaseHelper.FIREBASE_DATABASE_EMAIL_YTALERTS).getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(AboutActivity.this, R.string.error_loading_emails_about, Toast.LENGTH_LONG).show();
+            }
+        };
+
+        singleValueEventListener = new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(AboutActivity.this, R.string.error_loading_emails_about, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        };
+
+        about.addValueEventListener(valueEventListener);
+
+        about.addListenerForSingleValueEvent(singleValueEventListener);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // when the user clicks the back/home button...
+            case android.R.id.home:
+                // close this activity
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
